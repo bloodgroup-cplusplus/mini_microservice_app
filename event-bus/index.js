d8c6dbe1a496ff911fb60d25edf52d689d9659fc
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-09 19:02:26 +0530

    add post requests to middlewares

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,3 +9,12 @@ const app= express();
 
 app.use(bodyParser.json())
 
+app.post("/events", (req,res)=>{
+    const event = req.body
+    axios.post("http://localhost:400/events",event);
+    axios.post("http://localhost:4001/events",event);
+    axios.post("http://localhost:4002/events",event);
+
+
+
+})
