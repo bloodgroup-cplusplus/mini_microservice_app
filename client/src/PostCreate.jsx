bb00aa3ef632c1c7eecde16ada24d6536224069c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-09 14:31:51 +0530

    added cors

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -4,11 +4,21 @@ import axios from "axios";
 
 
 export default function PostCreate(){
+    const[title,setTitle] = useState('')
+
+    const onSubmit= async(event)=>{
+        event.preventDefault();
+        await axios.post('http://localhost:4000/posts',{
+            title
+        });
+        setTitle('');
+
+    }
     return (<div>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label>Title</label>
-                <input className="form-control"/>
+                <input value={title} onChange={e=>setTitle(e.target.value)} className="form-control"/>
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
