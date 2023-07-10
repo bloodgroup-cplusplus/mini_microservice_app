1dccfabd1055cbec18a5787cf6f099ef345ed810
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-10 09:31:47 +0530

    scaffolding project

---
+++ query/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const cors = require("cors")
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get("/posts", (req,res)=>{
+
+});
+
+
+app.post("/events",(req,res)=>{
+
+});
+
+
+app.listen(4002,()=>{
+    console.log('Server is running on port 4002');
+})
+
+
